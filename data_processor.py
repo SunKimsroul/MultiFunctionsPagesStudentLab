1f5a2d59829d3d68db480e64091f90fecf0aa3f3
altradCS <145883008+altradCS@users.noreply.github.com>
2024-02-22 20:28:08 +0700

    Create data_processor.py

---
+++ data_processor.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+def format_name(first_name, last_name):
+    
+    return 
+
+def calculate_age(birth_year):
+    """Calculates the user's age from the birth year."""
+    
+    return 
