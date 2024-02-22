b74a466e9babf41bec0f97b18486146e9641451c
altradCS <145883008+altradCS@users.noreply.github.com>
2024-02-22 20:29:39 +0700

    Create user_interface.py

---
+++ user_interface.py
@@ -0,0 +1,19 @@
+# user_interface.py
+
+from data_processor import format_name, calculate_age
+
+def get_user_details():
+  
+    return
+
+def get_contact_info():
+
+    return email, phone, age
+
+def display_greeting(name):
+  
+    print()
+
+def display_contact_info(email, phone, age):
+  
+    print()
